538364df7df46eaeffa1167f29c6a16b567df395
lucifer2355 <dgajjar999@gmail.com>
2021-08-02 23:02:27 +0530

    add different travelsal method

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const grandparent = document.getElementById("grandparent-id");
+const grandparentQuerySelector = document.querySelector(".grandparent");
+const parent = Array.from(document.getElementsByClassName("parent"));
+const parentQuerySelectorAll = document.querySelectorAll(".parent");
 
-changeColor(grandparent);
+const parents = Array.from(grandparentQuerySelector.children);
+const parentOne = parents[0];
+const children = parentOne.children;
+
+const childOne = document.querySelector(".child");
+const childrens = document.querySelectorAll(".child");
+
+const childOne1 = document.querySelector("#child-one");
+const childOne1Parent = childOne1.parentElement;
+const childOne1ParentGrandparent = childOne1Parent.parentElement;
+
+const childTwo = childOne.nextElementSibling;
+
+// changeColor(grandparent);
+// parent.forEach(changeColor);
+// changeColor(children[0]);
+// changeColor(childOne);
+// childrens.forEach(changeColor);
+// changeColor(childOne1Parent);
+// changeColor(childOne1ParentGrandparent);
+// changeColor(childTwo);
+changeColor(childTwo.previousElementSibling);
 
 function changeColor(element) {
   element.style.backgroundColor = "#333";
